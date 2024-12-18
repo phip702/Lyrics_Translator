4a2620270bd707d2cb845f333a6c4f8fa08e774f
Philip Ranney <philipranney@Philips-MacBook-Air-2.local>
2024-12-18 13:47:38 -0700

    Deleted all the coding files

--- run.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-from app import create_app
-import os
-
-app, celery = create_app()
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001)) # tells Flask to look for the PORT variable assigned by Heroku else use local port 5001
-    app.run(host='0.0.0.0', port = port) # 0.0.0.0 means run on all addresses
