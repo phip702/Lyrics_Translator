3ee27dd66c853f6b016654443be989e1161c433e
Philip Ranney <philipranney@Philips-MacBook-Air-2.local>
2025-01-03 03:31:49 -0700

    edited the front end code, mostly CSS. need to work on the layout separation between sidenav and main content still though.

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
     // Access the playlist ID from the data attribute of the button
     var spotify_playlist_id = $('#load-more-btn').data('playlist-id');
 
+    // Load more for the body
     $('#load-more-btn').click(function() {
         $.ajax({
             url: `/api/fetch_tracks/${spotify_playlist_id}?offset=${offset}`,
@@ -42,10 +43,58 @@ $(document).ready(function() {
         });
     });
 
+    // Sidenav Load More functionality
+    let sidenavOffset = 50;  // Start with an offset of 50 for the sidenav
+    var spotify_playlist_id_sidenav = $('#load-more-sidenav').data('playlist-id');
+
+    // Handle "Load More" for the sidenav
+    $('#load-more-sidenav').click(function() {
+        $.ajax({
+            url: `/api/fetch_tracks/${spotify_playlist_id_sidenav}?offset=${sidenavOffset}`,
+            method: 'GET',
+            success: function(response) {
+                if (response.length > 0) {
+                    // Append the new tracks to the sidenav's track list
+                    response.forEach(track => {
+                        $('#sidenav-tracks').append(`
+                            <li class="track-row" data-track-id="${track.spotify_track_id}">
+                                <img src="${track.track_image}" alt="Track Image" width="50" height="50">
+                                <div class="track-info">
+                                    <span class="track-name">${track.track_name}</span>
+                                    <span class="track-artist">${track.track_artist}</span>
+                                </div>
+                            </li>
+                        `);
+                    });
+
+                    // Increase the offset by 50 for the next request
+                    sidenavOffset += 50;
+
+                    // Update the rows count and check if it is a multiple of 50
+                    sidenavRowsCount = $('#sidenav-tracks li').length;
+
+                    if (sidenavRowsCount % 50 !== 0) {
+                        $('#load-more-sidenav').hide();  // Hide button if all tracks are loaded
+                    }
+                }
+            },
+            error: function(error) {
+                alert('Error fetching tracks. Please try again.');
+            }
+        });
+    });
+
     // reroute to track page when user clicks on a track row
     $(".track-row").on("click", function() {
         var spotify_track_id = $(this).find(".spotify-track-id").text();  // Get the track ID from the hidden column
         console.log("Track ID:", spotify_track_id);
+        window.location.href = "/track/" + spotify_track_id; // Redirect to track page
+    });
+
+    // Same for the sidenav track rows
+    $(".sidenav .track-row").on("click", function() {
+        var spotify_track_id = $(this).data("track-id");  // Get the track ID from the data attribute
+        console.log("Track ID from sidenav:", spotify_track_id);
         window.location.href = "/track/" + spotify_track_id; // Redirect to track page
     });
 });
