b9e9210824561557bae6d94ccfedbeaac1bb2277
Philip Ranney <philipranney@Philips-MacBook-Air-2.local>
2025-01-12 22:40:41 -0700

    Reverted my files back to how they were before trying to deploy to Heroku.

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,6 @@ from .routes.analytics import analytics
 #delete from .models import Track, Lyrics
 from dotenv import load_dotenv
 import os
-import pika
 from sqlalchemy import inspect
 from threading import Thread
 from app.services.rabbitmq.insert_row_producer import start_producer
@@ -18,8 +17,6 @@ from app.services.rabbitmq.insert_row_consumer import start_consumer
 def run_rabbitmq_services():
     producer_thread = Thread(target=start_producer)
     consumer_thread = Thread(target=start_consumer)
-    producer_thread.daemon = True  #* Mark as daemon so it doesn't block app termination
-    consumer_thread.daemon = True  #* Mark as daemon so it doesn't block app termination
     producer_thread.start()
     consumer_thread.start()
 
@@ -33,20 +30,15 @@ def create_app():
 
     app = Flask(__name__, static_url_path="", static_folder="static")
     app.secret_key = 'sjfa90u3214sfdfaJIS0324'  # Necessary for sessions
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite:///db.sqlite3")
-    logging.info(f"RABBITMQ: {os.getenv("DATABASE_URL", "sqlite:///db.sqlite3")}")
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    logging.info(f"RABBITMQ: { os.getenv("DATABASE_URL", "sqlite:///db.sqlite3")}")
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
-    rabbitmq_url = os.getenv("CLOUDAMQP_URL", "amqp://guest:guest@localhost:5672/")
-    app.config['RABBITMQ_URL'] = rabbitmq_url
+    app.config['RABBITMQ_URL'] = os.getenv('RABBITMQ_URL')
     logging.info(f"RABBITMQ: {os.getenv("CLOUDAMQP_URL", "amqp://guest:guest@localhost:5672/")}")
-    connection = pika.BlockingConnection(pika.URLParameters(rabbitmq_url)) #*
-    channel = connection.channel()
-
     db.init_app(app)
     migrate.init_app(app,db)
 
@@ -65,3 +57,22 @@ def create_app():
 
     print("APP INITIALIZED")
     return app
+
+
+
+#C-TODO: message queue
+#C-TODO: implement async via threading for producing and consuming
+#C-TODO: playlist click track -> track page
+#C-TODO: integration testing
+#C-TODO: unit testing
+#C-TODO: analytics --just use my old artifact, don't do anything more
+#TODO: reset requirement.txt to remove things like Celery
+#C-TODO: front end (HTML template/polymorphism, CSS)
+#C-TODO: Add buffering/loading indication
+#TODO: live deploy
+#TODO: continuous integration?
+#TODO: continuous delivery?
+#TODO: production monitoring instrumenting
+
+#TODO: make genius api call ignore 'romanized' versions; enforce language selection by user
+#C-TODO: make playlist become side menu after user selects track
