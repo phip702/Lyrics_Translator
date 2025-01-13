de408dc1cebd30aa498795a1ed55b72e64e2f2c2
Philip Ranney <philipranney@Philips-MacBook-Air-2.local>
2025-01-12 19:11:27 -0700

    Remove venv directory from version control

--- app/__init__.py
+++ app/__init__.py
@@ -30,14 +30,14 @@ def create_app():
 
     app = Flask(__name__, static_url_path="", static_folder="static")
     app.secret_key = 'sjfa90u3214sfdfaJIS0324'  # Necessary for sessions
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite:///db.sqlite3")
 
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    #app.app_context().push()
+    app.config['RABBITMQ_URL'] = os.getenv("CLOUDAMQP_URL", "amqp://guest:guest@localhost:5672/")
 
     db.init_app(app)
     migrate.init_app(app,db)
